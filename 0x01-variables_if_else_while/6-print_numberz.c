760bdabf8b29a005189cb4b9de18ff49d3be3654
Ifeoma Ukwu <ukwushannon@gmail.com>
2022-12-11 09:25:22 -0800

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print all single digits of base 10
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int digit = 0;
+
+	while (digit <= 9)
+	{
+		putchar(digit + '0');
+		++digit;
+	}
+	putchar('\n');
+	return (0);
+}
