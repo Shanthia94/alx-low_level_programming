643b6a0eea1a1f3b6b42816e79b6d850174c7ac7
Ifeoma Ukwu <ukwushannon@gmail.com>
2022-12-15 15:12:56 -0800

    The numbers speak for themselves

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * print_numbers - Entry point
@@ -13,7 +14,7 @@ void print_numbers(void)
 
 	for (c = '0'; c <= '9'; c++)
 	{
-	_putchar(c);
+	putchar(c);
 	}
-	_putchar('\n');
+	putchar('\n');
 }
