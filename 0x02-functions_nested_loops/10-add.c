8e130869a11b50aa4163d8aa9cff559c216ee4bb
Ifeoma Ukwu <ukwushannon@gmail.com>
2022-12-14 21:39:56 -0800

    a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - Entry point
+ * @a: the first number
+ * @b: the second number
+ * Description: add two integers and print th result
+ *
+ * Return: the result
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
