8633cd215ae74bb49727aac43b31a45e93adf66e
Ifeoma Ukwu <ukwushannon@gmail.com>
2022-12-14 20:14:47 -0800

    Learn your times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -16,13 +16,11 @@ void times_table(void)
 	for (num = 0; num <= 9; num++)
 	{
 		_putchar('0');
-	}
 	for (mult = 1; mult <= 9; mult++)
 	{
 		_putchar(',');
 		_putchar(' ');
 		prod = num * mult;
-	}
 	if (prod <= 9)
 		_putchar(' ');
 	else
@@ -31,4 +29,6 @@ void times_table(void)
 		_putchar((prod % 10) + '0');
 	}
 		_putchar('\n');
+	}
+	}
 }
