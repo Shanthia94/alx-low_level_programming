0bacba75544c1a4f6f14bfa52c849b88dec0da66
Ifeoma Ukwu <ukwushannon@gmail.com>
2022-12-15 14:33:18 -0800

    Triangles

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_triangle - Entry point
+ * @size: size ofnthe triangle
+ * Description: print a traingle 
+ *
+ * Return: void
+ */
+void print_triangle(int size)
+{
+	int row, hashes, spaces;
+
+	if (size <= 0)
+	{
+		putchar('\n');
+	}
+	else
+	{
+		for (row = 1; row <= size; row++)
+		{
+			for (spaces = size - row; spaces >= 1; spaces--)
+			{
+			putchar(' ');
+			}
+		for (hashes = 1; hashes <= row; hashes++)
+		{
+		putchar('#');
+		}
+	putchar('\n');
+		}
+	}
+}	
+
