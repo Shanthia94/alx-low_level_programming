75be2c002c9a4e17c32426c65492584651d05213
Ifeoma Ukwu <ukwushannon@gmail.com>
2022-12-22 14:38:18 -0800

    Expect the best. Prepare for the worst. Capitalize on what comes

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -14,13 +14,13 @@ char *cap_string(char *s)
 	{
 		if (*(s + i) >= 'a' && *(s + i) <= 'z')
 		{
-			if (i = 0)
+			if (i == 0)
 				*(s + i) -= 'a' - 'A';
 			else
 			{
 				for (j = 0; j <= 12; j++)
 				{
-					if (a[j] == *(s + i + 1))
+					if (a[j] == *(s + i - 1))
 						*(s + i) -= 'a' - 'A';
 				}
 			}
