ca669b5e037ae458574dba1d1461a259cb4a7256
Shanthia94 <ukwushannon@gmail.com>
2023-03-24 10:10:37 -0700

    Dynamic Libraries

---
+++ 0x18-dynamic_libraries/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - Entry point
+ * @t: the interger to be generated
+ * Decription: a funtion that computes absolute value of an integer
+ *
+ * Return: Absolute valu
+ */
+int _abs(int t)
+{
+	if (t >= 0)
+		return (t);
+	else
+		return (-t);
+}
